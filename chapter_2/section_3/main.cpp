714f6911773a090a5ff035c342eb30740459603f
kk <kamaihamaiha@gmail.com>
2022-05-11 22:44:36 +0800

    update: 复合类型之引用

---
+++ chapter_2/section_3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/**
+ * 引用
+ */
+int main(){
+
+  int i = 0, &r1 = i;
+  double d = 0, &r2 = d;
+
+  r2 = 3.14;
+  r2 = r1;
+
+  // 练习 2.17. 输出结果为 10, 10
+  int j, &rj = j;
+  j = 5;
+  rj = 10;
+
+  std::cout << j << ", " << rj << std::endl;
+  return 0;
+}
